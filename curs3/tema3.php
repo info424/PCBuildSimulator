5e0e169214c0a515a5a2fae28dde81ea7934d099
info424 <istunt3rro@gmail.com>
2024-04-18 00:29:22 +0300

    Update tema3.php

--- curs3/tema3.php
+++ curs3/tema3.php
@@ -10,43 +10,40 @@ function operatiiMatematice($a, $b) {
     ];
 
     foreach ($operatii as $op => $rezultat) {
-        echo "Rezultatul operatiei de $op este: $rezultat<br>";
+        echo "Rezultatul operatiei de $op este: $rezultat";
     }
 
     $maxVal = max($operatii);
     $maxOp = array_search($maxVal, $operatii);
     echo "Operatia care produce cel mai mare rezultat este: $maxOp cu valoarea $maxVal";
 }
-?>
 
 
-
-<?php
 function tablaInmultirii($n) {
     if ($n >= 0 && $n <= 10) {
         for ($i = 0; $i <= 10; $i++) {
-            echo "$n x $i = " . ($n * $i) . "<br>";
+            echo "$n x $i = " . ($n * $i);
         }
     } else {
         echo "Parametrul nu se află în intervalul 0 - 10.";
     }
 }
-?>
 
 
-<?php
+
+
 function comparatii($x, $y) {
-    echo "$x este " . ($x < $y ? "mai mic" : "nu este mai mic") . " decât $y<br>";
-    echo "$x este " . ($x <= $y ? "mai mic sau egal" : "nu este mai mic sau egal") . " cu $y<br>";
-    echo "$x " . ($x == $y ? "este egal" : "nu este egal") . " cu $y<br>";
-    echo "$x este " . ($x >= $y ? "mai mare sau egal" : "nu este mai mare sau egal") . " cu $y<br>";
-    echo "$x este " . ($x > $y ? "mai mare" : "nu este mai mare") . " decât $y<br>";
+    echo "$x este " . ($x < $y ? "mai mic" : "nu este mai mic") . " decât $y";
+    echo "$x este " . ($x <= $y ? "mai mic sau egal" : "nu este mai mic sau egal") . " cu $y";
+    echo "$x este " . ($x == $y ? "este egal" : "nu este egal") . " cu $y";
+    echo "$x este " . ($x >= $y ? "mai mare sau egal" : "nu este mai mare sau egal") . " cu $y";
+    echo "$x este " . ($x > $y ? "mai mare" : "nu este mai mare") . " decât $y";
 }
-?>
 
 
 
-<?php
+
+
 function operatieConditii($param1, $param2, $param3) {
     $param2 = $param2 <= 0 ? 1 : $param2;
     $param3 = $param3 <= 0 ? 2 : $param3;
@@ -54,7 +51,7 @@ function operatieConditii($param1, $param2, $param3) {
     if (($param1 - $param2) > 5 && ($param3 % 2) == 0) {
         echo "Rezultatul este: " . (pow(($param1 / $param2), $param2));
     } else {
-        echo "Condițiile nu sunt îndeplinite.";
+        echo "rip.";
     }
 }
 ?>
