f010c2fecfedb9dd7da39b253ba2f53fa3d07e19
info424 <istunt3rro@gmail.com>
2024-05-14 19:18:57 +0300

    uppy

---
+++ curs10/tema100.php
@@ -0,0 +1,9 @@
+<?php
+//
+//require_once('index.php');
+//
+//$produs1 = new Produs('Telefon', 1500);
+//
+//echo $produs1->toString();
+//
+//
---
+++ curs11/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once ('Book.php');
+
+use Library\Book;
+
+$book = new Book();
+var_dump($book);
+
